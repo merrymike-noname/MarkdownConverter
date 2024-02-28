8578375fa41ef01dd0bbdf3df64cde0ea63dd441
Mykhailo Kovalenko <kovalenko.mykhailo1@lll.kpi.ua>
2024-02-28 19:40:49 +0200

    Add basic italic replacement algorithm

--- src/main/java/org/merrymike/soft/Main.java
+++ src/main/java/org/merrymike/soft/Main.java
@@ -1,7 +1,21 @@
 package org.merrymike.soft;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String input = "This_is _italic_ text. _This_ is _more_italic_ text.";
+        Pattern pattern = Pattern.compile("\\b_(.*?)_\\b");
+        Matcher matcher = pattern.matcher(input);
+
+        StringBuffer replacedString = new StringBuffer();
+        while (matcher.find()) {
+            String italicText = matcher.group(1);
+            matcher.appendReplacement(replacedString, "<i>" + italicText + "</i>");
+        }
+        matcher.appendTail(replacedString);
+
+        System.out.println(replacedString);
     }
 }
